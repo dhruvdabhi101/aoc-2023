27a26c2e66032099f52534ab116bb952f1bfa9e4
Dhruv <dhruvdabhi101@gmail.com>
2023-12-02 14:32:29 +0530

    added day 2 part 1

--- day2/target/debug/day2.d
+++
@@ -1,1 +0,0 @@
-/Users/dhruvdabhi/temp/aoc2023/day2/target/debug/day2: /Users/dhruvdabhi/temp/aoc2023/day2/src/main.rs
--- day2/target/debug/deps/day2-a097168bfe8b7ce8.d
+++
@@ -1,5 +0,0 @@
-/Users/dhruvdabhi/temp/aoc2023/day2/target/debug/deps/day2-a097168bfe8b7ce8.rmeta: src/main.rs
-
-/Users/dhruvdabhi/temp/aoc2023/day2/target/debug/deps/day2-a097168bfe8b7ce8.d: src/main.rs
-
-src/main.rs:
